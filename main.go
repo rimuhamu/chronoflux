39a347b45f068853b05fd0bb146ca2e4cb5fb2da
Emir Risyad <emirrsyd@gmail.com>
2026-03-02 20:05:06 +0100

    feat: implement wprker pool and rate limiter

--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+type Task interface {
+	Execute(ctx context.Context) error
+}
+
+type EmailTask struct {
+	ID int
+}
+
+func (t *EmailTask) Execute(ctx context.Context) error {
+	fmt.Println("executing task", t.ID)
+	return nil
+}
+
+func worker(id int, tasks <-chan Task, results chan<- int, r *rate.Limiter) {
+	for task := range tasks {
+		r.Wait(context.Background())
+		task.Execute(context.Background())
+		fmt.Println("worker", id, "started job", task)
+		time.Sleep(time.Second)
+		fmt.Println("worker", id, "finished job", task)
+		results <- id
+	}
+}
 
 func main() {
-	fmt.Println("Hello Chronoflux")
+
+	var wg sync.WaitGroup
+	var r = rate.NewLimiter(rate.Every(time.Second), 1)
+
+	const numTasks = 5
+	tasks := make(chan Task, numTasks)
+	results := make(chan int, numTasks)
+
+	for w := 1; w <= 3; w++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			worker(id, tasks, results, r)
+		}(w)
+	}
+	for j := 1; j <= numTasks; j++ {
+		tasks <- &EmailTask{ID: j}
+	}
+	close(tasks)
+
+	wg.Wait()
+	close(results)
+	for a := 1; a <= numTasks; a++ {
+		fmt.Println("result: ", <-results)
+	}
 }
